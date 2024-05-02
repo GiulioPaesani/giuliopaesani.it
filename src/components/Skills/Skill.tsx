792c60bbb7be9f140388dbfa8dbb9c6616e5800d
GiulioAndCode <giulioandcode@gmail.com>
2024-05-02 12:14:44 +0200

    Skills hover effects

--- src/components/Skills/Skill.tsx
+++ src/components/Skills/Skill.tsx
@@ -5,11 +5,11 @@ type SkillProps = {
 
 const Skill = ({ label, icon }: SkillProps) => {
 	return (
-		<div id='competenze' className=' w-64 h-36 relative mb-4'>
+		<div id='competenze' className=' w-64 h-36 relative mb-4 group ease-in-out duration-200 hover:-translate-y-3'>
 			<div className='bg-primary-500 w-full h-[104px] absolute bottom-0 rounded-lg'></div>
 			<div className='w-full h-full absolute bottom-0 '>
-				<img src={`./${icon}`} alt={label} className='m-auto' />
-				<p className='text-3xl text-neutral-50 text-center my-4'>{label}</p>
+				<img src={`./${icon}`} alt={label} className='m-auto ease-in-out duration-200 group-hover:-rotate-6 group-hover:-translate-y-1' />
+				<p className='text-3xl text-neutral-50 text-center my-4 ease-in-out duration-200 group-hover:rotate-3 group-hover:-translate-y-1'>{label}</p>
 			</div>
 		</div>
 	);
