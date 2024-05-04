97d9ec38e11cd35bbeaa9668fce01fe3c824e277
GiulioAndCode <giulioandcode@gmail.com>
2024-05-04 10:59:45 +0200

    Fix  responsive

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
 	return (
 		<div className='w-full h-20 flex items-center sticky top-0 bg-neutral-50/80 z-50'>
 			<NavbarOptions />
-			<ContactMeButton className='ml-auto lg:ml-12' side='right' />
+			<ContactMeButton className='mr-auto sm:mr-0 sm:ml-auto lg:ml-12' side='right' />
 		</div>
 	);
 };
