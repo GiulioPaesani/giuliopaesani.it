2db76020aa22fc77fb0d6ca8351c9ef182d0a29c
GiulioAndCode <giulioandcode@gmail.com>
2024-05-13 11:30:02 +0200

    Fix navbar values order

--- src/components/Navbar/NavbarOptions.tsx
+++ src/components/Navbar/NavbarOptions.tsx
@@ -3,7 +3,7 @@ import NavbarOption from './NavbarOption';
 const NavbarOptions = () => {
 	return (
 		<div className='items-center ml-auto gap-6 hidden lg:flex'>
-			{['Competenze', 'Istruzione', 'Progetti', 'Contatti'].map((option, index) => (
+			{['Competenze', 'Progetti', 'Istruzione', 'Contatti'].map((option, index) => (
 				<NavbarOption option={option} index={index} key={index} />
 			))}
 		</div>
