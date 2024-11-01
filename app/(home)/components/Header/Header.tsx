ec77b2cac05f0856a9eb8b5784cf2585238f8cd7
GiulioAndCode <giulioandcode@gmail.com>
2024-11-01 16:27:55 +0100

    Remove hiring text

--- app/(home)/components/Header/Header.tsx
+++ app/(home)/components/Header/Header.tsx
@@ -1,13 +1,13 @@
 import ContactMeButton from '@/app/components/Button/ContactMeButton';
 import Title from '@/app/components/Header/Title';
-import HiringText from './HiringText';
+// import HiringText from './HiringText';
 import Subtitle from './Subtitle';
 import Button from '@/app/components/Button/Button';
 
 const Header = () => {
 	return (
 		<div className='w-full'>
-			<HiringText />
+			{/* <HiringText /> */}
 			<Title text='Giulio Paesani' text2='Ciao, io sono' />
 			<Subtitle />
 
